dafd3edf42bdd2724b6af918cac384932fd3e5a4
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-06-11 19:49:03 +0430

    TodoApp added in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TodoApp from '@components/TodoApp'
+import TodoApp from '@/components/TodoApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //ensure that developers follow the React best practices
   <React.StrictMode>
     <TodoApp />
   </React.StrictMode>,
