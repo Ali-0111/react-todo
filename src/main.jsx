17209b0d4b1b2faf863680fd4c4099973e38a60b
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-06-12 10:09:36 +0430

    Styles added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TodoApp from '@/components/TodoApp'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TodoApp from '@/components/TodoApp';
+import '@/styles/app.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //ensure that developers follow the React best practices
